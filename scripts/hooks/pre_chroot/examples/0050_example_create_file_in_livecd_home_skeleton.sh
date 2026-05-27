18eac4061f48aae1bdda60bbaa7097d8cb875a9d
Robert Bast <rebast85@gmail.com>
2026-05-27 21:48:24 +0200

    Added more explanation to example pre-chroot hook script.

--- scripts/hooks/pre_chroot/examples/0050_example_create_file_in_livecd_home_skeleton.sh
+++ scripts/hooks/pre_chroot/examples/0050_example_create_file_in_livecd_home_skeleton.sh
@@ -2,6 +2,13 @@
 
 # This is a pre-chroot hook example file. Copy it and adapt it to your wishes.
 
+# This example creates a file in the user home skeleton.
+# You can do a lot more with /etc/skel/.
+
+# You can copy over configuration files from your running desktop
+# to the user home skeleton to have your settings applied in the live iso
+# and an installation from that live iso as well.
+
 # Create directory chroot/etc/skel/Desktop and create a file in it
 mkdir -p "${SCRIPT_DIR}/chroot/etc/skel/Desktop"
 touch "${SCRIPT_DIR}/chroot/etc/skel/Desktop/file.txt"
